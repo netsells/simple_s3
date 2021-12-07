7c047f029e9ffcbff85c65d2bbed8ef0c6fe553d
Helena Sewell <helena.sewell@netsells.co.uk>
2021-12-07 13:25:24 +0000

    set content length to too high (for my test video)

--- android/src/main/java/com/abhimortal6/simple_s3/SimpleS3Plugin.java
+++ android/src/main/java/com/abhimortal6/simple_s3/SimpleS3Plugin.java
@@ -142,7 +142,7 @@ public class SimpleS3Plugin implements FlutterPlugin, MethodCallHandler, EventCh
         ObjectMetadata objectMetadata = new ObjectMetadata();
         System.out.println(fileName.substring(fileName.lastIndexOf(".")+1));
         objectMetadata.setContentType(contentType);
-        objectMetadata.setContentLength(1L);
+        objectMetadata.setContentLength(500000L);
 
 
         CannedAccessControlList acl;
